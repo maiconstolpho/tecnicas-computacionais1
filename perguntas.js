c98fb5ae3e9659311a46395c5325456390c7a3d9
maiconstolpho <105317762+maiconstolpho@users.noreply.github.com>
2024-08-16 11:28:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,158 @@
+export const perguntas = [
+    {
+        enunciado: "REVENDA AUTORIZADA CAMAGRIL NO QUE POSSO AUXILIAR?",
+        alternativas: [
+            {
+                texto: "VENDA DE EMPLEMENTOS ",
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: " MF PÓS VENDAS ",
+                afirmacao: [
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quando o maquinário começa a ficar lento e com necessidade de mais manutenção do que o normal, pode prejudicar a produtividade e a demanda pode cair. Por isso, a produtividade deve ser o principal objetivo para a troca de máquinas",
+        alternativas: [
+            {
+                texto: "trator",
+                afirmacao: [
+                    "até 100 cv",
+                    "maior de 100 cv",
+                    "média de 100 cv"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "coilheitadeira ",
+                afirmacao: [
+                    "procuro compras de pequeno porte", 
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Nosso compromisso é fornecer produtos da mais alta qualidade e um serviço excepcional. ",
+        alternativas: [
+            {
+                texto: "PROCURO TRATOR,COLHEITADEIRA",
+                afirmacao: [
+                    "De grande porte",
+                    "Medio porte.",
+                    "."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "PROCURO OUTRO TIPO DE MAQUINÁRIO",
+                afirmacao: [ 
+                    "usados",
+                    "novos.",
+                    "quero outro maquinario"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual maquinario você esta precisando?",
+        alternativas: [
+            {
+                texto: "trator e colheitadeira",
+                afirmacao: [
+                    "pulverização e colheita",
+                    "procuro saber mais sobre formas de pagamento"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "OUTRO MAQUINÁRIO",
+                afirmacao: [
+                    " A VISTA"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "formas de pagamento",
+        alternativas: [
+            {
+                texto: "financiamento",
+                afirmacao: [
+                    " á vista ",
+                    "Em safras"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "trocar meu maquinario usado",
+                afirmacao: [
+                    "Sempre precisando de um novo maquinario para ajudar na produção "
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "ATENDIMENDO PELO EMAIL DO RESPONSAVEL QUE RESPONDEU ESSE QUESTONARIO",
+        alternativas: [
+            {
+                texto: "PREFIRO ATENDIMENTO ONLINE",
+                afirmacao: [
+                    "procuro maquina mais tecnologica"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "PREFIRO ATENDIMENTO PRESENCIAL",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Um atendente da nossa loja ira entrar em contato ",
+        alternativas: [
+            {
+                texto: "Prefiro atendimento presencial.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Prefiro atendimento presencial",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    
